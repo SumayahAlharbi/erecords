127bf02cab7b854a4ca4808a8ee46243f10e7248
SumayahAlharbi <ftooo-ff@hotmail.com>
2018-09-04 15:22:17 +0300

    Revert "Merge branch 'ExportFeature'"
    This reverts commit 1e52aed89c4b45a49563e972542a60c28cb448f6, reversing
    changes made to 0c99b8264a0d4a39b6d223b21906781dc8115e01.

--- app/Student.php
+++ app/Student.php
@@ -6,16 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-  public $table='students';
-  protected $fillable =[
-    'FirstName',
-    'LastName',
-    'Badge',
-    'NationalID',
-    'Status',
-    'StudentNo',
-    'Batch',
-    'Stream'
-  ];
 
 }
