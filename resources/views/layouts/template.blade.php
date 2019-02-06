c24eea0ceac97abc5f90db7f5509701ba30731a2
SumayahAlharbi <ftooo-ff@hotmail.com>
2019-02-06 12:43:04 +0300

    Remove jQuery Slim lib
    Conflict with jQuery min lib

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -11,7 +11,8 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   -->
   <link href="{{ asset('css/bootstrap-min.css') }}" media="all" rel="stylesheet" type="text/css">
-  <!--<link href="{{ asset('css/fontawesome-all.css') }}" media="all" rel="stylesheet" type="text/css">-->
+  <!-- Dose not work
+  <link href="{{ asset('css/fontawesome-all.css') }}" media="all" rel="stylesheet" type="text/css">-->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
   <link href="{{ asset('css/custom-nav.css') }}" media="all" rel="stylesheet" type="text/css">
@@ -104,14 +105,13 @@
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-  <script type="text/javascript" src="{{ asset('js/jquery-3-3-1-slim-min.js') }}"></script>
+  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
+  <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+
   <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>-->
   <script type="text/javascript" src="{{ asset('js/popper-min.js') }}"></script>
   <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>-->
   <script type="text/javascript" src="{{ asset('js/bootstrap-min.js') }}"></script>
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
-  <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
 
   <!--<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet"/>-->
   <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
