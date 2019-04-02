f5d19155b4b7b209989f0b8df24b00dd426aeda5
Somayah <somayah.alharbi@yahoo.com>
2019-04-02 11:51:19 +0300

    Login Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,109 +1,87 @@
 @extends('layouts.template')
 @section('content')
-<section class="section" style="padding:50px;min-height: 100vh;">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
+<section class="login" style="background-image:url({{ asset('logo/home.jpg')}});min-height: 100vh;background-size: cover;
+background-repeat: no-repeat;
+background-position: center center;
+height: 100%;
+width: 100%;
+padding: 8% 0;
+position: fixed;">
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
 
-        <!-- Nav tabs -->
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-          <li class="nav-item">
-            <a class="nav-link active" id="CAS-tab" data-toggle="tab" href="#cas" role="tab" aria-controls="CAS Login" aria-selected="true">CAS Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="App Login" aria-selected="false">App Login</a>
-          </li>
-        </ul>
+      <div class="card" style="  width: 350px;
+      margin: 0 auto;">
+      <div class="card-body">
 
-        <!-- Tab panes -->
-        <div class="tab-content">
 
-          <div class="tab-pane active" id="cas" role="tabpanel" aria-labelledby="CAS-tab">
-            <div class="card">
-            <div class="card-body">
-              <div class="form-group text-center m-t-20">
-                <div class="col-xs-12">
-                  <!--<h5> Comming Soon </h5>
-                  <h6> Login with KSAU-HS ID </h6>-->
-                  <a class="btn btn-primary btn-lg waves-effect waves-light" href="{{ url('cas/login')}}">{{ __('Login with KSAU-HS ID') }}</a>
-                </div>
-              </div>
+
+        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+          @csrf
+
+          <div class="form-group text-center m-t-20">
+            <div class="col-xs-12">
+              <a class="btn btn-lg waves-effect waves-light btn-success btn-block" href="{{ url('cas/login')}}">{{ __('Login with KSAU-HS ID') }}</a>
             </div>
           </div>
-        </div>
-          <div class="tab-pane" id="login" role="tabpanel" aria-labelledby="login-tab">
 
-            <div class="card">
-              <div class="card-body">
-                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                  @csrf
+          <hr>
+          <h3 class="box-title m-b-20">Login</h3>
 
-                  <div class="form-group row">
-                    <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+          <div class="form-group">
+            <div class="col-xs-12">
+              <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-                    <div class="col-md-6">
-                      <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                      @if ($errors->has('email'))
-                      <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('email') }}</strong>
-                      </span>
-                      @endif
-                    </div>
-                  </div>
-
-                  <div class="form-group row">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                    <div class="col-md-6">
-                      <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                      @if ($errors->has('password'))
-                      <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('password') }}</strong>
-                      </span>
-                      @endif
-                    </div>
-                  </div>
-
-                  <div class="form-group row">
-                    <div class="col-md-6 offset-md-4">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                        <label class="form-check-label" for="remember">
-                          {{ __('Remember Me') }}
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="form-group row mb-0">
-                    <div class="col-md-8 offset-md-4">
-                      <button type="submit" class="btn btn-primary">
-                        {{ __('Login') }}
-                      </button>
-
-                      <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('Forgot Your Password?') }}
-                      </a>
-                    </div>
-                  </div>
-                </form>
-              </div>
+              @if ($errors->has('email'))
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('email') }}</strong>
+              </span>
+              @endif
             </div>
-
           </div>
-        </div>
+
+          <div class="form-group ">
+            <div class="col-xs-12">
+              <input id="password" placeholder="Password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+              @if ($errors->has('password'))
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('password') }}</strong>
+              </span>
+              @endif
+            </div>
+          </div>
+
+          <div class="form-group ">
+            <div class="col-xs-12">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                <label class="form-check-label" for="remember">
+                  {{ __('Remember Me') }}
+                </label>
+              </div>
+              <i class="fas fa-lock"></i><a class="btn btn-link" href="{{ route('password.request') }}">
+                {{ __('Forgot Your Password?') }}
+              </a>
+            </div>
+          </div>
+
+          <div class="form-group text-center m-t-20">
+            <div class="col-xs-12">
+              <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Login</button>
+            </div>
+          </div>
 
 
-
-
+        </form>
       </div>
-
-
     </div>
+
+
   </div>
+</div>
 </div>
 </section>
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -28,21 +28,24 @@
   <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
     <div class="container">
       <a class="navbar-brand" href="{{ route('home') }}"><img src={{ asset('logo/comj-logo.png')}} alt='COMJ Logo' height="87" width="385" id="Logo"></a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+
+      <!--<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fa fa-bars"></i>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav text-uppercase ml-auto">
-          <!--<li class="nav-item">
+          @role('admin')
+          <li class="nav-item">
             <a class="nav-link" href="{{ route('register') }}">Register</a>
-          </li>-->
+          </li>
+          @endrole
           <li class="nav-item">
             <a class="nav-link" href="{{ route('login') }}">Login</a>
           </li>
         </ul>
-      </div>
+      </div>-->
     </div>
   </nav>
 
